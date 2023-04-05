14ba4198246973d150ee82743e65dcd0d3efb95b
homegrownotes <homegrownotes@gmail.com>
2023-04-05 12:50:59 +0200

    Exercici 2

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -9,6 +9,10 @@ const Container = styled.div`
 
 export const Checkbox = ({ data, onChange }) => {
   const [total, setTotal] = useState(0); // => Define "price state".
+  const [numPages, setNumPages] = useState(0); // => Define "price pages state".
+  const [numLanguages, setNumLanguages] = useState(0); // => Define "price languages state".
+  const [showExtraOptions, setShowExtraOptions] = useState(false); // => Define "extras state".
+  const [resetExtras, setResetExtras] = useState(false); // => Define "reset extras state".
 
   const handleCheckboxChange = (e) => {
     const {
@@ -16,12 +20,35 @@ export const Checkbox = ({ data, onChange }) => {
       dataset: { price, id },
     } = e.target; // => Destructuring to clean "code".
 
+    if (id === "1") {
+      // => The value getted by "dataset" is a string, not a number.
+      setShowExtraOptions(isChecked);
+      if (!isChecked) {
+        setResetExtras(true);
+      }
+    }
+
     if (isChecked) {
       setTotal((prevTotal) => prevTotal + parseFloat(price));
+      if (id === "1") {
+        setResetExtras(false);
+      }
     } else {
       setTotal((prevTotal) => prevTotal - parseFloat(price));
     }
   };
+
+  const handleNumPagesChange = (e) => {
+    const { value } = e.target;
+    setNumPages(value);
+  };
+
+  const handleNumLanguagesChange = (e) => {
+    const { value } = e.target;
+    setNumLanguages(value);
+  };
+
+  const extraOptionsPrice = resetExtras ? 0 : numPages * numLanguages * 30;
 
   return (
     <Container>
@@ -38,11 +65,42 @@ export const Checkbox = ({ data, onChange }) => {
                 data-id={e.id}
               />
               {e.option} ({e.price} €)
+              {showExtraOptions && e.id === 1 && (
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    borderStyle: "solid",
+                    borderRadius: "15px",
+                    padding: "35px 20px",
+                    margin: "10px",
+                  }}
+                >
+                  <label style={{ paddingBottom: "15px" }}>
+                    Número de páginas:
+                    <input
+                      type="number"
+                      min="0"
+                      value={numPages}
+                      onChange={handleNumPagesChange}
+                    />
+                  </label>
+                  <label>
+                    Número de idiomas:
+                    <input
+                      type="number"
+                      min="0"
+                      value={numLanguages}
+                      onChange={handleNumLanguagesChange}
+                    />
+                  </label>
+                </div>
+              )}
             </label>
           </div>
         ))}
       <div>
-        <p> Precio Total: {total} €</p>
+        <p> Precio Total: {total + extraOptionsPrice} €</p>
       </div>
     </Container>
   );
